fcb69a407efd0281a8729dcbe7f1c5b93238af14
ianMonteiro07 <ian.monteiro2018@gmail.com>
2025-08-11 18:35:17 -0300

    solução concluída do invertendo a lista

---
+++ invertendo_a_lista.py
@@ -0,0 +1,11 @@
+def invertendo(lista):
+    lista2=[]
+    for numero in reversed(lista):
+        lista2.append(numero)
+    print(lista2)
+
+
+
+#pp
+lista=[987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+invertendo(lista)
