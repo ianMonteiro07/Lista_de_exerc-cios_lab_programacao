1349d693292d960e755dd78d4cad3b552e3408a3
ianMonteiro07 <ian.monteiro2018@gmail.com>
2025-08-11 18:28:23 -0300

    solução concluída do Encontrando os numeros impare

---
+++ encontrando_os_numeros_impares.py
@@ -0,0 +1,9 @@
+def pares(lista):
+    nova=lista[0:]
+    for i in nova:
+        if i%2==0:
+            nova.remove(i)
+    print(f'lista impar -> {nova}' )
+lista=[987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+print(f'lista original -> {lista}')
+pares(lista)
